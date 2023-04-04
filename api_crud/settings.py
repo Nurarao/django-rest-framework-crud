cc8748b3728aa96eddf8045c064f2c1953ae8511
Arunrao <arunrao@LAP-REPOS-231.local>
2023-04-04 14:04:38 +0530

    changes

--- api_crud/settings.py
+++ api_crud/settings.py
@@ -133,4 +133,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
   STATIC_URL = '/static/'
-#  STATIC_ROOT = 'manage.py' 
+  STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
